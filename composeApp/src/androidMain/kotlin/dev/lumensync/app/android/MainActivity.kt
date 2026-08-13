57e2f5792bbc3a55f184b0edf1ddc62b3a5061f6
Decade-GitHub <soap1808@gmail.com>
2026-08-13 13:32:30 -0300

    Android padding fix

--- composeApp/src/androidMain/kotlin/dev/lumensync/app/android/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/lumensync/app/android/MainActivity.kt
@@ -3,6 +3,8 @@ package dev.lumensync.app.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import dev.lumensync.app.ui.LumenSyncApp
 import kotlinx.coroutines.launch
@@ -16,6 +18,16 @@ class MainActivity : ComponentActivity() {
         val platform = AndroidPlatformActions(this)
         lifecycleScope.launch { graph.initialize() }
         setContent { LumenSyncApp(graph.engine, platform) }
+
+        val content = findViewById<android.view.View>(android.R.id.content)
+        ViewCompat.setOnApplyWindowInsetsListener(content) { view, insets ->
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout(),
+            )
+            view.setPadding(view.paddingLeft, bars.top, view.paddingRight, bars.bottom)
+            insets
+        }
+        ViewCompat.requestApplyInsets(content)
     }
 
     override fun onStop() {
